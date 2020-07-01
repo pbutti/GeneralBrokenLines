41115a887881282b038aba4e311cd256bb733865
pbutti <pierfrancesco.butti@gmail.com>
2020-07-01 09:17:58 -0700

    GblTrajectoryWrapper

---
+++ cpp/src/GblTrajectoryWrapper.cpp
@@ -0,0 +1,71 @@
+#include "GblTrajectory.h"
+
+using namespace gbl;
+using namespace Eigen;
+
+
+
+
+
+extern "C" {
+    
+    GblTrajectory* GblTrajectoryCtor(int flagCurv, int flagU1dir, int flagU2dir) {
+        
+        return new GblTrajectory(flagCurv, flagU1dir, flagU2dir);
+        
+    }
+        
+    //Simple trajectory constructor wrapper
+    GblTrajectory* GblTrajectoryCtorPtrArray(GblPoint* points[], int npoints, 
+                                             int flagCurv, int flagU1dir, int flagU2dir) {
+        
+        
+        std::vector<GblPoint> aPointList;
+        
+        for (int i=0; i<npoints; i++) {
+            //get the point pointer
+            GblPoint* gblpoint = points[i];
+            
+            //add it to the vector
+            aPointList.push_back(*(gblpoint));
+        }
+        
+        return new GblTrajectory(aPointList, flagCurv, flagU1dir, flagU2dir);
+    }
+
+    //Simple trajectory constructor with seed wrapper
+    
+    GblTrajectory* GblTrajectoryCtorPtrArraySeed(GblPoint* points[], int npoints,
+                                                 int aLabel, double seedArray[],
+                                                 int flagCurv, int flagU1dir, int flagU2dir) {
+        
+        std::vector<GblPoint> aPointList;
+        
+        for (int i=0; i<npoints; i++) {
+            //get the point pointer
+            GblPoint* gblpoint = points[i];
+            
+            //add it to the vector
+            aPointList.push_back(*(gblpoint));
+        }
+        
+        Map<Matrix5d> seed(seedArray,5,5);
+        
+        return new GblTrajectory(aPointList, aLabel, seed, flagCurv, flagU1dir, flagU2dir);
+        
+    }
+    
+    void GblTrajectory_fit(GblTrajectory* self, double* Chi2, int* Ndf, double* lostWeight, char* c_optionList, unsigned int aLabel) {
+        
+        std::string optionList(c_optionList);
+        self->fit(*Chi2, *Ndf, *lostWeight, optionList,aLabel);
+    }
+    
+    int GblTrajectory_isValid(GblTrajectory* self) {
+        
+        return (int) self->isValid();
+    }
+}
+
+
+
